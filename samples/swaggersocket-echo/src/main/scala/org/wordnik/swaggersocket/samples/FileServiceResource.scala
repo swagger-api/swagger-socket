bd51dc9f5f659b937cdb0c959d1c475363dd104d
jfarcand <jfarcand@apache.org>
2012-04-16 11:44:21 -0400

    Fix package name

--- samples/swaggersocket-echo/src/main/scala/org/wordnik/swaggersocket/samples/FileServiceResource.scala
+++ samples/swaggersocket-echo/src/main/scala/org/wordnik/swaggersocket/samples/FileServiceResource.scala
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.wordnik.swaggersocket.samples
+package com.wordnik.swaggersocket.samples
 
 import java.io.InputStream
 import javax.servlet.ServletContext
--- samples/swaggersocket-echo/src/main/scala/org/wordnik/swaggersocket/samples/SwaggerSocketResource.scala
+++ samples/swaggersocket-echo/src/main/scala/org/wordnik/swaggersocket/samples/SwaggerSocketResource.scala
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.wordnik.swaggersocket.samples
+package com.wordnik.swaggersocket.samples
 
 import org.slf4j.LoggerFactory
 import javax.ws.rs.{Produces, POST, Path}
